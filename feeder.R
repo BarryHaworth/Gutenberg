182f7d37aaa1b5c02ec19682a74e4c3ced5b1f07
BarryHaworth <barryhaworth@gmail.com>
2020-11-20 20:45:19 +1000

    Working Parse - first cut

--- feeder.R
+++
@@ -1,9 +0,0 @@
-# Read the RDF/XML files usinfg feedR
-
-library(feedeR)
-
-PROJECT_DIR <- "c:/R/Gutenberg"
-DATA_DIR    <- paste0(PROJECT_DIR,"/data")
-RDF_DIR     <- paste0(DATA_DIR,"/cache/epub")
-
-rdf_file <- paste0(RDF_DIR,"/5230/pg5230.rdf")  # The Invisible Man
