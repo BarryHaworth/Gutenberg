98237a8f89f9ff6881b6a3885c8f59e7bec9dc99
BarryHaworth <barryhaworth@gmail.com>
2021-10-23 12:14:13 +1000

    Added Librivox read

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+---
+output:
+  html_document: default
+  word_document: default
+---
 # Gutenberg
 Code to read and process the Project Gutenberg catalog
+
+The purpose of this repository is to read the Project Gutenberg catalog
+and parse it into a usable form.  At a minimum, the intent is to create
+a dataframe of books, consisting of:
+- Title
+- Author
+- Publication Date (or year)
+- Language
+- Subject
+- Release Date
+- Gutenberg book number
+- Gutenberg Link
+
+Other useful information:
+- Size of book file
+- SF flag
+- Audio flag
+- Librivox link (if available)
+- Other information from Librivox
+
+My goal in this is to help me find suitable texts to read for Librivox, hence the
+links with Librivox (to identify what hasn't yet been recorded), 
+length (to find shorter books) and the SF flag (my personal interest)
--- librivox.R
+++ librivox.R
@@ -1,16 +1,53 @@
-# Librivox Collections
-# Short Science Fiction Collections
-# https://librivox.org/short-science-fiction-collection-vol-001-by-various/
-# These currently go from number 1 to number 81.
+# Code to read the Librivox catalog
+# Should perhaps for in something other than my Gutenberg repo,
+# But for now here it is.
+#
+# Discussion thread about the Librivox API can be found on this discussion thread:
+#
+# https://forum.librivox.org/viewtopic.php?f=24&t=44129
+#
+# The API itself is here:
+#  https://librivox.org/api/info
+
+# Get a single book:
+# https://librivox.org/api/feed/audiobooks/?id=52
+
+# Default format is xml.  To get JSON:
+
+# https://librivox.org/api/feed/audiobooks/?id=52&format=json
+#
+# ID currently goes from 52 to 
+# https://librivox.org/api/feed/audiobooks/?id=17234
+
+# This code loops through librivox files from 1 to 18000 and saves the results
+# as json and xml files.
 
 library(rvest)
 library(dplyr)
 library(xml2)
 
-PROJECT_DIR <- "c:/R/Gutenberg"
-DATA_DIR    <- "c:/R/Gutenberg/data"
+PROJECT_DIR <- "c:/R/Gutenberg/"
+DATA_DIR    <- paste0(PROJECT_DIR,"data/")
+LB_DIR      <- paste0(DATA_DIR,"librivox/")
 
-url <- "https://librivox.org/short-science-fiction-collection-vol-001-by-various/"
+ 
+url <- "https://librivox.org/api/feed/audiobooks/?id=52"
+local_file <- paste0(LB_DIR,"librivox52.xml")
+download.file(url,local_file)
+url <- "https://librivox.org/api/feed/audiobooks/?id=52&format=json"
+local_file <- paste0(LB_DIR,"librivox52.json")
+download.file(url,local_file,quiet=TRUE)
 
-webpage <- read_html(url)
-details <- xml_attrs(html_nodes(webpage,'td:nth-child(3) , a'))
+for (i in seq(1,18000)){
+  print(paste("Downloading file number",i))
+  url_x   <- paste0("https://librivox.org/api/feed/audiobooks/?id=",i)
+  local_x <- paste0(LB_DIR,"librivox_",i,".xml")
+  url_j   <- paste0("https://librivox.org/api/feed/audiobooks/?id=",i,"&format=json")
+  local_j <- paste0(LB_DIR,"librivox_",i,".json")
+  tryCatch({
+    download.file(url_x,local_x,quiet=TRUE)
+    download.file(url_j,local_j,quiet=TRUE)}
+    ,error = function(e) print(paste("Librivox file",i,"does not work exist"))
+    )
+}
+
