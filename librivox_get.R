96d29fd19ab231bfbf601ca2917abd89bbed3e52
BarryHaworth <barryhaworth@gmail.com>
2021-10-23 15:58:03 +1000

    Librivox Fetch and parse

--- librivox_get.R
+++ librivox_get.R
@@ -20,7 +20,7 @@
 # https://librivox.org/api/feed/audiobooks/?id=17234
 
 # This code loops through librivox files from 1 to 18000 and saves the results
-# as json and xml files.
+# as  xml files. (Decided not to use json)
 
 library(rvest)
 library(dplyr)
@@ -30,24 +30,22 @@ PROJECT_DIR <- "c:/R/Gutenberg/"
 DATA_DIR    <- paste0(PROJECT_DIR,"data/")
 LB_DIR      <- paste0(DATA_DIR,"librivox/")
 
- 
 url <- "https://librivox.org/api/feed/audiobooks/?id=52"
-local_file <- paste0(LB_DIR,"librivox52.xml")
+#url <- "https://librivox.org/api/feed/audiobooks"
+#local_file <- paste0(LB_DIR,"librivox52.xml")
+local_file <- paste0(LB_DIR,"librivox.xml")
 download.file(url,local_file)
-url <- "https://librivox.org/api/feed/audiobooks/?id=52&format=json"
-local_file <- paste0(LB_DIR,"librivox52.json")
-download.file(url,local_file,quiet=TRUE)
 
-for (i in seq(1,18000)){
-  print(paste("Downloading file number",i))
+for (i in seq(47,18000)){
   url_x   <- paste0("https://librivox.org/api/feed/audiobooks/?id=",i)
   local_x <- paste0(LB_DIR,"librivox_",i,".xml")
-  url_j   <- paste0("https://librivox.org/api/feed/audiobooks/?id=",i,"&format=json")
-  local_j <- paste0(LB_DIR,"librivox_",i,".json")
-  tryCatch({
-    download.file(url_x,local_x,quiet=TRUE)
-    download.file(url_j,local_j,quiet=TRUE)}
-    ,error = function(e) print(paste("Librivox file",i,"does not work exist"))
+  if (!file.exists(local_x)){
+    print(paste("Downloading file number",i))
+    tryCatch({
+      download.file(url_x,local_x,quiet=TRUE)
+      }
+      ,error = function(e) print(paste("Librivox file",i,"does not exist"))
     )
+  } else {print(paste(local_x,"already exists"))}
 }
 
---
+++ librivox_parse.R
@@ -0,0 +1,61 @@
+# Parse the Librivox files
+
+library(dplyr)
+library(XML)
+
+PROJECT_DIR <- "c:/R/Gutenberg/"
+DATA_DIR    <- paste0(PROJECT_DIR,"data/")
+LB_DIR      <- paste0(DATA_DIR,"librivox/")
+
+# Load the previous database and create if not present.
+if (file.exists(paste0(DATA_DIR,"/librivox.RData"))){
+  load(paste0(DATA_DIR,"/librivox.RData"))
+} else librivox = data.frame()
+
+# Get a list of files
+librivox <- dir(LB_DIR)
+lb_json  <- librivox[grepl("json",librivox)]
+lb_xml   <- librivox[grepl("xml",librivox)]
+
+# Parse the results
+
+#i=3
+#for (i in seq(1,10)){
+for (i in seq(1,length(lb_xml))){
+  data <- xmlParse(paste0(LB_DIR,lb_xml[i]))
+  xml_data <- xmlToList(data)[[1]]$book
+  
+  id              <- as.numeric(xml_data$id)
+  title           <- toString(xml_data$title)
+  description     <- toString(xml_data$description)
+  language        <- toString(xml_data$language)
+  copyright_year  <- as.numeric(xml_data$copyright_year)
+  num_sections    <- as.numeric(xml_data$num_sections)
+  url_librivox    <- toString(xml_data$url_librivox)
+  url_text_source <- toString(xml_data$url_text_source)
+  url_zip_file    <- toString(xml_data$url_zip_file)
+  url_rss         <- toString(xml_data$url_rss)
+  url_project     <- toString(xml_data$url_project)
+  url_other       <- toString(xml_data$url_other)
+  totaltime       <- toString(xml_data$totaltime)
+  totaltimesecs   <- as.numeric(xml_data$totaltimesecs)
+  author_id         <- as.numeric(xml_data$authors[1]$author$id)
+  author_first_name <- toString(xml_data$authors[1]$author$first_name)
+  author_last_name  <- toString(xml_data$authors[1]$author$last_name)
+  author_dob        <- max(0,as.numeric(xml_data$authors[1]$author$dob))
+  author_dod        <- max(0,as.numeric(xml_data$authors[1]$author$dod))
+  
+  print(paste("Book #",i,title,"by",author_first_name,author_last_name))
+  
+  book=data.frame(id,title,description,language,copyright_year,num_sections,
+                  url_librivox,url_text_source,
+                  totaltime,totaltimesecs,
+                  author_id,author_first_name,author_last_name,author_dob,author_dod,
+                  stringsAsFactors = FALSE,fix.empty.names = FALSE)
+  
+  librivox <- rbind(librivox,book)
+}
+
+librivox <- librivox %>% unique()
+
+save(librivox,file=paste0(DATA_DIR,"/librivox.RData"))
